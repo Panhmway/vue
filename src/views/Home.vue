87388a214abed1a016ee7590973e9ff3b09e2ef1
Panhmway <jokejoke186@gmail.com>
2021-08-17 16:57:52 +0630

    test

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+          <div v-if="error">
+              {{error}}
+          </div>
+          <div v-if="posts.length>0">
+             <PostsList :posts="posts"></PostsList>
+          </div>
+          <div v-else>
+              loading...
+          </div>
+       
+    
+  </div>
+</template>
+
+<script>
+import PostsList from '../components/PostsList'
+import getPosts from "../composables/getPosts"
+
+export default {
+  components: { PostsList },
+    setup(){
+      // composable function
+      let {posts,error,load}=getPosts()//{posts,error,load}
+      
+      load();
+
+      return {posts,error};
+    }
+}
+</script>
